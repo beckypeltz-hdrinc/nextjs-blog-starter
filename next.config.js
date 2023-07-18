8d5bae94f131050707dfc58b6dfef20d3b64ae80
beckypeltz-hdrinc <becky.peltz@hdrinc.com>
2023-07-18 09:55:09 -0700

    set base path for github pages

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+  
+   // Add basePath
+    basePath: '/nextjs-blog-starter',
+  
+  }
+  
+  module.exports = nextConfig
